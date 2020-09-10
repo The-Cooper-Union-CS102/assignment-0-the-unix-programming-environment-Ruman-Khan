84932a6fdeea597e71b66f78598fb3c4f9048090
Ruman-Khan <70612742+Ruman-Khan@users.noreply.github.com>
2020-09-09 20:12:20 -0400

    Update solution.sh

--- solution.sh
+++ solution.sh
@@ -2,9 +2,9 @@ mkdir mydir
 cd mydir
 mkdir mydir2
 cd mydir2
-touch myfile
-echo "Hello Bash" > myfile
-cat myfile
+touch myfile.txt
+echo "Hello Bash" > myfile.txt
+cat myfile.txt
 cd ..
 cp -r mydir2 mydir3
 find . | sort -r
